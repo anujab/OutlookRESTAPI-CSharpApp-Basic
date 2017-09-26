bee2bebeaf59079b1c67738ee7ba5ed3dadf0897
anujab <anujab@ANUJA-SURFACE>
2017-09-25 23:02:54 -0400

    Parsing JSON response

---
+++ OutlookRESTAPI-CSharpApp-Basic/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookRESTAPI_CSharpApp_Basic
+{
+    class Constants
+    {
+        internal const string OutlookRestapiEndpoint = "https://outlook.office.com/api/v2.0/me/messages?$top=1";
+
+        internal static readonly string[] Scopes = new string[] { "https://outlook.office.com/mail.read" };
+    }
+}
--- OutlookRESTAPI-CSharpApp-Basic/Program.cs
+++ OutlookRESTAPI-CSharpApp-Basic/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace OutlookRESTAPI_CSharpApp_Basic
 {
@@ -10,18 +11,14 @@ namespace OutlookRESTAPI_CSharpApp_Basic
     {
         static void Main(string[] args)
         {
-            //Set the API Endpoint to Graph 'me' endpoint
-            var outlookRESTAPIEndpoint = "https://outlook.office365.com/api/v2.0/me/messages?$top=1";
-
-            //Set the scope for API call to user.read
-            var scopes = new string[] { "https://outlook.office.com/mail.read" };
-
-
-            string authToken = AuthHelper.GetAuthToken(scopes);
+            string authToken = AuthHelper.GetAuthToken(Constants.Scopes);
 
             if (authToken != null)
             {
-                Console.WriteLine(HttpHelper.SendAsync(outlookRESTAPIEndpoint, authToken));
+                string response = HttpHelper.SendAsync(Constants.OutlookRestapiEndpoint, authToken);
+                dynamic responseObject = JObject.Parse(response);
+                var mailitem = responseObject.value[0];
+                Console.WriteLine("Id : " + mailitem.Id + "\nSubject : " + mailitem.Subject + "\nBody : " + mailitem.Body);
             }
         }
     }
